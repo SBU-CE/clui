a023231750ea59808b843fcfe18acafe2fe84754
rsharifnasab@gmail.com <rsharifnasab@gmail.com>
2020-06-08 17:52:21 +0430

    removed junk function from sample.c

--- sample.c
+++ sample.c
@@ -1,24 +1,5 @@
 #include "clui.h"
 
-/*
-int kbhit()
-{
-    struct termios term;
-    tcgetattr(0, &term);
-
-    struct termios term2 = term;
-    term2.c_lflag &= ~ICANON;
-    tcsetattr(0, TCSANOW, &term2);
-
-    int byteswaiting;
-    ioctl(0, FIONREAD, &byteswaiting);
-
-    tcsetattr(0, TCSANOW, &term);
-
-    return byteswaiting > 0;
-}
-*/
-
 int main() {
     // initialize library
     init_clui();
