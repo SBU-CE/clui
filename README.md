245154f7b21dcc11d12a1972ffd4217e97102bf3
roozbeh sharifnasab <rsharifnasab@gmail.com>
2020-06-08 17:21:15 +0430

    completed sample from older version

--- README.md
+++ README.md
@@ -2,5 +2,8 @@
 simple multi-platform library for doing terminal thing.
 
 to learn more about functions, view `sample.c` 
-it is guranteed that library work on both windows and linux with C or C++
+
+
+### it is guranteed that library work on both windows and linux with C or C++
+
 
--- sample.c
+++ sample.c
@@ -20,10 +20,29 @@ int kbhit()
 */
 
 int main() {
+    // initialize library
     init_clui();
-    while (1==1) {
-        //printf("we are not blocking\n");
-        //flush();
+
+    // example of using delay function
+    // input is an integer showing time of delay 
+    // in milliseconds
+    printf("testing delay\nit should take 5 seconds\n");
+    delay(5000); // wait for 5 seconds
+   
+
+    // get size of terminal with 2 functions for rows and cols
+    printf("number of terminal cols: %d\n",  get_window_cols() );
+    printf("number of terminal rows: %d\n",  get_window_rows() );
+
+
+    // change print color 
+    // with RGB
+    change_color_rgb(150,10,15);
+    printf("this text is some color \n");
+    reset_color();
+
+
+    while (true) {
         printf(".");
         flush();
         delay(1000);
@@ -34,5 +53,7 @@ int main() {
         }
     }
 
+
+    quit();
     return 0;
 } 
