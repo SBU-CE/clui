f7c9e33073a2fc136c1ba4d760e460facbf6c07b
Parsa Nouri <parsanouri@Parsas-MacBook-Pro.local>
2020-12-23 16:11:27 +0330

    Added get_pos some macros and a bit of encapsulation

--- clui.h
+++ clui.h
@@ -5,18 +5,16 @@
  * @rsharifnasab
  */
 
-
 #ifndef _CLUI
 #define _CLUI
 
 /*
  * common includes
  */
-#include <stdio.h>
-#include <stdlib.h>
 #include <signal.h>
 #include <stdbool.h>
-
+#include <stdio.h>
+#include <stdlib.h>
 
 /*
  * includes for windows
@@ -24,8 +22,8 @@
 #if defined(_WIN32) || defined(_WIN64) || defined(WI32)
 
 #define OS_UNIX 0
-#include <conio.h>
 #include <Windows.h>
+#include <conio.h>
 
 /*
  * includes for unix-like systems
@@ -34,88 +32,99 @@
 #else
 
 #define OS_UNIX 1
+#include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
-#include <sys/ioctl.h>
 
 #endif
-
 
 /*
  * define colors that can be chosen
  * in change_color function
  */
-#define COLOR_DEFAULT      0
-#define COLOR_RED          1
-#define COLOR_BOLD_RED     2
-#define COLOR_GREEN        3
-#define COLOR_BOLD_GREEN   4
-#define COLOR_YELLOW       5
-#define COLOR_BOLD_YELLOW  6
-#define COLOR_BLUE         7
-#define COLOR_BOLD_BLUE    8
-#define COLOR_MAGENTA      9
+#define COLOR_DEFAULT 0
+#define COLOR_RED 1
+#define COLOR_BOLD_RED 2
+#define COLOR_GREEN 3
+#define COLOR_BOLD_GREEN 4
+#define COLOR_YELLOW 5
+#define COLOR_BOLD_YELLOW 6
+#define COLOR_BLUE 7
+#define COLOR_BOLD_BLUE 8
+#define COLOR_MAGENTA 9
 #define COLOR_BOLD_MAGENTA 10
-#define COLOR_CYAN         11
-#define COLOR_BOLD_CYAN    12
+#define COLOR_CYAN 11
+#define COLOR_BOLD_CYAN 12
 
 /*
  * colors added in version2
  * this is use 8 bit terminal
  */
-#define COLOR_ORANGE_1    13
-#define COLOR_ORANGE_2    14
-#define COLOR_ORANGE_3    15
+#define COLOR_ORANGE_1 13
+#define COLOR_ORANGE_2 14
+#define COLOR_ORANGE_3 15
 
+/*
+ * ANSI escape sequences
+ * Visit:
+ * http://ascii-table.com/ansi-escape-sequences.php
+ * for more details
+ */
+#define cursorup(n) printf("\033[%dA", (n))
+#define cursordown(n) printf("\033[%dB", (n))
+#define cursorforward(n) printf("\033[%dC", (n))
+#define cursorbackward(n) printf("\033[%dD", (n))
+#define cursortopos(r, c) printf("\033[%d;%dH", (r), (c))
+#define savecursorpos() printf("\0337")
+// below macro restores cursor to the last
+// saved position via above macro
+#define restorecursorpos() printf("\0338")
+#define playbeep() printf("\07")
+#define ansescape() printf("\033[c")
 
-
-
-void enable_raw_mode(){
+static void enable_raw_mode()
+{
 #if OS_UNIX
-    struct termios term;
-    tcgetattr(0, &term);
-    term.c_lflag &= ~(ICANON | ECHO); // Disable echo as well
-    tcsetattr(0, TCSANOW, &term);
+	struct termios term;
+	tcgetattr(0, &term);
+	term.c_lflag &= ~(ICANON | ECHO); // Disable echo as well
+	tcsetattr(0, TCSANOW, &term);
 #else
 
 #endif
 }
 
-void disable_raw_mode(){
+static void disable_raw_mode()
+{
 #if OS_UNIX
-    struct termios term;
-    tcgetattr(0, &term);
-    term.c_lflag |= ICANON | ECHO;
-    tcsetattr(0, TCSANOW, &term);
+	struct termios term;
+	tcgetattr(0, &term);
+	term.c_lflag |= ICANON | ECHO;
+	tcsetattr(0, TCSANOW, &term);
 #else
 
 #endif
 }
 
-
-
-bool is_keyboard_hit(){
+bool is_keyboard_hit()
+{
 #if OS_UNIX
-    int byteswaiting;
-    ioctl(0, FIONREAD, &byteswaiting);
-    return byteswaiting > 0;
+	int byteswaiting;
+	ioctl(0, FIONREAD, &byteswaiting);
+	return byteswaiting > 0;
 #else
-    return kbhit();
+	return kbhit();
 #endif
 }
 
-
-
-
-void clear_screen(){
+void clear_screen()
+{
 #if OS_UNIX
-    system("clear");
+	system("clear");
 #else
-    system("CLS");
+	system("CLS");
 #endif
 }
-
-
 
 /*
  * change output color of terminal
@@ -124,122 +133,152 @@ void clear_screen(){
  * tested in linux terminals
  * and new Windows10 cmd and powershell
  */
-void change_color_rgb(int r, int g, int b){
-    printf("\033[38;2;%d;%d;%dm",r,g,b);
+void change_color_rgb(int r, int g, int b)
+{
+	printf("\033[38;2;%d;%d;%dm", r, g, b);
 }
 
 /*
  * change color in 8 bit terminal
  */
-void change_color(int color) {
-    printf("\033");
+void change_color(int color)
+{
+	printf("\033");
 
-    const char *colors[] = {
-        "[0m",
-        "[0;31m",
-        "[1;31m",
-        "[0;32m",
-        "[1;32m",
-        "[0;33m",
-        "[1;33m",
-        "[0;34m",
-        "[1;34m",
-        "[0;35m",
-        "[1;35m",
-        "[0;36m",
-        "[1;36m",
+	const char* colors[] = {
+		"[0m",
+		"[0;31m",
+		"[1;31m",
+		"[0;32m",
+		"[1;32m",
+		"[0;33m",
+		"[1;33m",
+		"[0;34m",
+		"[1;34m",
+		"[0;35m",
+		"[1;35m",
+		"[0;36m",
+		"[1;36m",
 
-        "[38;5;202m",
-        "[38;5;208m",
-        "[38;5;214m"
-    };
-    if (color >= 0 && color <= 15) {
-        printf("%s", colors[color]);
-    }
+		"[38;5;202m",
+		"[38;5;208m",
+		"[38;5;214m"
+	};
+	if (color >= 0 && color <= 15) {
+		printf("%s", colors[color]);
+	}
 }
 
-
-void reset_color(){
-    change_color(COLOR_DEFAULT);
+void reset_color()
+{
+	change_color(COLOR_DEFAULT);
 }
 
-
-void flush(){
-    fflush(stdout);
-    fflush(stderr);
+void flush()
+{
+	fflush(stdout);
+	fflush(stderr);
 }
 
-
-void quit() {
-    reset_color();
-    disable_raw_mode();
-    clear_screen();
-    exit(0);
+void quit()
+{
+	reset_color();
+	disable_raw_mode();
+	clear_screen();
+	exit(0);
 }
 
-
-void sigint_handler(int dummy) {
-    quit();
+void sigint_handler(int dummy)
+{
+	quit();
 }
 
-void init_clui(){
-    clear_screen();
-    signal(SIGINT, sigint_handler);
-    enable_raw_mode();
+void init_clui()
+{
+	clear_screen();
+	signal(SIGINT, sigint_handler);
+	enable_raw_mode();
 }
-
 
 #if OS_UNIX
-int getch(){
-    struct termios oldattr, newattr;
-    int ch;
-    tcgetattr(STDIN_FILENO, &oldattr);
-    newattr = oldattr;
-    newattr.c_lflag &= ~(ICANON | ECHO);
-    tcsetattr(STDIN_FILENO, TCSANOW, &newattr);
-    ch = getchar();
-    tcsetattr(STDIN_FILENO, TCSANOW, &oldattr);
-    return ch;
+int getch()
+{
+	struct termios oldattr, newattr;
+	int ch;
+	tcgetattr(STDIN_FILENO, &oldattr);
+	newattr = oldattr;
+	newattr.c_lflag &= ~(ICANON | ECHO);
+	tcsetattr(STDIN_FILENO, TCSANOW, &newattr);
+	ch = getchar();
+	tcsetattr(STDIN_FILENO, TCSANOW, &oldattr);
+	return ch;
 }
 #endif
 
-
-void delay(int milli_seconds){
+void delay(int milli_seconds)
+{
 #if OS_UNIX
-    usleep(1000*milli_seconds);
+	usleep(1000 * milli_seconds);
 #else
-    Sleep(milli_seconds);
+	Sleep(milli_seconds);
 #endif
 }
 
-
-
-
-int get_window_rows(){
+int get_window_rows()
+{
 #if OS_UNIX
-    struct winsize max;
-    ioctl(0, TIOCGWINSZ , &max);
-    return max.ws_row;
+	struct winsize max;
+	ioctl(0, TIOCGWINSZ, &max);
+	return max.ws_row;
 #else
-    CONSOLE_SCREEN_BUFFER_INFO csbi;
-    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
-    return csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+	GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+	return csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
 #endif
 }
 
-
-int get_window_cols(){
+int get_window_cols()
+{
 #if OS_UNIX
-    struct winsize max;
-    ioctl(0, TIOCGWINSZ , &max);
-    return max.ws_col;
+	struct winsize max;
+	ioctl(0, TIOCGWINSZ, &max);
+	return max.ws_col;
 #else
-    CONSOLE_SCREEN_BUFFER_INFO csbi;
-    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
-    return csbi.srWindow.Right - csbi.srWindow.Left + 1;
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+	GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+	return csbi.srWindow.Right - csbi.srWindow.Left + 1;
 #endif
 }
 
+int get_pos(int* y, int* x)
+{
 
+	char buf[30] = { 0 };
+	int ret, i, pow;
+	char ch;
+
+	*y = 0;
+	*x = 0;
+
+	//asking for position via ANSI
+	//escape sequence
+	write(1, "\033[6n", 4);
+
+	for (i = 0, ch = 0; ch != 'R'; i++)
+		if (!read(0, buf + i, 1))
+			return 1;
+
+	if (i < 2)
+		return 1;
+
+	// parsing the output
+	for (i -= 2, pow = 1; buf[i] != ';'; i--, pow *= 10)
+		*x = *x + (buf[i] - '0') * pow;
+
+	for (i--, pow = 1; buf[i] != '['; i--, pow *= 10)
+		*y = *y + (buf[i] - '0') * pow;
+
+	return 0;
+}
 
 #endif
